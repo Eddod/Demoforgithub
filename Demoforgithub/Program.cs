4ab67987b26079e897c1a6ed5e18453811a2ee20
edwin <edwin@LAPTOP-GVBM1500>
2021-10-05 09:03:18 +0200

    Add project files.

---
+++ Demoforgithub/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Demoforgithub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
